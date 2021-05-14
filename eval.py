f61d61607f4df7059d69d14408ec675111d5d0cd
MrHoseongLee <49748733+MrHoseongLee@users.noreply.github.com>
2021-05-14 16:31:26 +0900

    Modified eval.py to compare more than 2 policies at once

--- eval.py
+++ eval.py
@@ -1,5 +1,6 @@
 import gym
 import argparse
+import itertools
 import torch as T
 
 from glob import glob
@@ -33,7 +34,7 @@ def BulitIn(net, path, STEP, STEP_MUL, END_IDX, BI_CNT):
 
             frameCount += 1
 
-            if frameCount >= 1200: done = True; reward = 0;
+            if frameCount >= 900: done = True; reward = 0;
 
             if done: 
                 isPlayer2Serve = not isPlayer2Serve
@@ -51,31 +52,26 @@ def BulitIn(net, path, STEP, STEP_MUL, END_IDX, BI_CNT):
     print()
     return elos
 
-def Eval(RA, RB, END_IDX, STEP, STEP_MUL, BI_CNT, GAME_CNT):
-    pathA = 'Saves/Run{:02d}/'.format(RA)
-    pathB = 'Saves/Run{:02d}/'.format(RB)
+def Eval(RS, END_IDX, STEP, STEP_MUL, BI_CNT, GAME_CNT):
+    RS.sort()
+    eval_id = '-'.join('{:02d}'.format(R) for R in RS)
 
-    algo_nameA = glob(pathA + '*.json')[0].split('-')[-1].split('.')[0]
-    algo_nameB = glob(pathB + '*.json')[0].split('-')[-1].split('.')[0]
+    N, nets, paths, elos = len(RS), [], [], []
 
-    algoA = __import__('Saves.Run{:02d}.Model'.format(RA), fromlist=[None])
-    algoB = __import__('Saves.Run{:02d}.Model'.format(RB), fromlist=[None])
+    for R in RS:
+        path = 'Saves/Run{:02d}/'.format(R)
 
-    netA = algoA.Model()
-    netB = algoB.Model()
+        net = __import__('Saves.Run{:02d}.Model'.format(R), fromlist=[None]).Model()
+        net.eval()
 
-    netA.eval()
-    netB.eval()
+        path = path + 'Models/'
 
-    pathA += f'Models/'
-    pathB += f'Models/'
+        nets.append(net)
+        paths.append(path)
+        elos.append(BulitIn(net, path, STEP, STEP_MUL, END_IDX, BI_CNT))
 
-    elosA = BulitIn(netA, pathA, STEP, STEP_MUL, END_IDX, BI_CNT)
-    elosB = BulitIn(netB, pathB, STEP, STEP_MUL, END_IDX, BI_CNT)
-
-    with open('Results/ELOC-{:02d}-{:02d}.txt'.format(RA, RB), 'w') as f:
-        for eloA, eloB in zip(elosA, elosB):
-            f.write(f'{eloA},{eloB}\n')
+    with open(f'Results/ELOC-{eval_id}.txt', 'w') as f:
+        for elo in elos: f.write(','.join(str(e) for e in elo) + '\n')
 
     env = gym.make('gym_pikachu_volleyball:pikachu-volleyball-v0',
             isPlayer1Computer=False, isPlayer2Computer=False)
@@ -85,47 +81,57 @@ def Eval(RA, RB, END_IDX, STEP, STEP_MUL, BI_CNT, GAME_CNT):
     observation = T.tensor(env.reset(isPlayer2Serve), dtype=T.float32)
 
     for idx, V in enumerate(range(STEP * STEP_MUL, END_IDX, STEP * STEP_MUL)):
-
         print(f'\rCompare at version {V}', end='')
 
-        netA.load_state_dict(T.load(pathA + '{:08d}.pt'.format(V)))
-        netB.load_state_dict(T.load(pathB + '{:08d}.pt'.format(V)))
+        elos_mean = [ 0 ] * N
 
-        gameCount, frameCount = 0, 0
+        for a in range(N): 
+            for b in range(a + 1, N):
+                netA, pathA, eloA = nets[a], paths[a], elos[a][idx]
+                netB, pathB, eloB = nets[b], paths[b], elos[a][idx]
 
-        while True:
-            with T.no_grad(): 
-                p1, *_ = netA(observation[0])
-                p2, *_ = netB(observation[1])
+                netA.load_state_dict(T.load(pathA + '{:08d}.pt'.format(V)))
+                netB.load_state_dict(T.load(pathB + '{:08d}.pt'.format(V)))
 
-            a1 = T.argmax(p1)
-            a2 = T.argmax(p2)
+                gameCount, frameCount = 0, 0
 
-            observation, reward, done, _ = env.step((a1, a2))
+                while True:
+                    with T.no_grad(): 
+                        p1, *_ = netA(observation[0])
+                        p2, *_ = netB(observation[1])
 
-            observation = T.tensor(observation, dtype=T.float32)
+                    a1 = T.argmax(p1)
+                    a2 = T.argmax(p2)
 
-            frameCount += 1
+                    observation, reward, done, _ = env.step((a1, a2))
 
-            if frameCount >= 1200: done = True; reward = 0;
+                    observation = T.tensor(observation, dtype=T.float32)
 
-            if done: 
-                isPlayer2Serve = not isPlayer2Serve
-                observation = T.tensor(env.reset(isPlayer2Serve), dtype=T.float32)
+                    frameCount += 1
 
-                frameCount = 0; gameCount += 1;
+                    if frameCount >= 900: done = True; reward = 0;
 
-                reward = (reward + 1) / 2
+                    if done: 
+                        isPlayer2Serve = not isPlayer2Serve
+                        observation = T.tensor(env.reset(isPlayer2Serve), dtype=T.float32)
 
-                elosA[idx], elosB[idx] = updateELO(elosA[idx], elosB[idx], 1 - reward, reward)
+                        frameCount = 0; gameCount += 1;
 
-                if gameCount >= GAME_CNT: break
+                        reward = (reward + 1) / 2
+
+                        elo = updateELO(eloA, eloB, 1 - reward, reward)
+
+                        elos_mean[a] += elo[0]
+                        elos_mean[b] += elo[1]
+
+                        if gameCount >= GAME_CNT: break
+
+        for i in range(N): elos[i][idx] = elos_mean[i] / (N - 1)
 
     print()
 
-    with open('Results/ELOF-{:02d}-{:02d}.txt'.format(RA, RB), 'w') as f:
-        for eloA, eloB in zip(elosA, elosB):
-            f.write(f'{eloA},{eloB}\n')
+    with open(f'Results/ELOF-{eval_id}.txt', 'w') as f:
+        for elo in elos: f.write(','.join(str(e) for e in elo) + '\n')
             
 def updateELO(RA, RB, SA, SB):
     EA = 1 / (1 + 10 ** ((RB - RA) / 400))
@@ -135,15 +141,14 @@ def updateELO(RA, RB, SA, SB):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--runA', type=int)
-    parser.add_argument('--runB', type=int)
+    parser.add_argument('--runs', nargs='*', type=int)
 
-    parser.add_argument('--end', type=int, default=int(1e8))
-    parser.add_argument('--step', type=int, default=16000)
+    parser.add_argument('--end', type=int, default=int(5e8))
+    parser.add_argument('--step', type=int, default=6400)
     parser.add_argument('--step_mul', type=int, default=10)
     parser.add_argument('--bi_cnt', type=int, default=100)
-    parser.add_argument('--game_cnt', type=int, default=200)
+    parser.add_argument('--game_cnt', type=int, default=100)
 
     args = parser.parse_args()
-    Eval(args.runA, args.runB, args.end, args.step, args.step_mul, args.bi_cnt, args.game_cnt)
+    Eval(args.runs, args.end, args.step, args.step_mul, args.bi_cnt, args.game_cnt)
 
